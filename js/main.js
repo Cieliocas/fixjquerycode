a2a428dbb45f41d7ea2b4ee92df52557c3f0c17b
Cieliocas <francielioevangelista@hotmail.com>
2024-08-23 03:59:59 -0300

    Revert "checking empty fields"
    This reverts commit 68190643130a393f644c315529d8c62b839b5820.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,43 @@
-$(document).ready(function () {
-    // Inicializa o carousel com slick
-    $('#carousel-images').slick({
-        autoplay: true,
-        arrows: false
-    });
 
-    // Máscaras para os campos
-    $('#telefone').mask('(00) 00000-0000', {
-        placeholder: '(DDD) 12345-6789'
-    });
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+})
 
-    $('#cpf').mask('000.000.000-00', {
-        placeholder: '123.456.789-00'
-    });
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+})
 
-    $('#cep').mask('00000-000', {
-        placeholder: '01234-567'
-    });
+$('#cep').mask('00000-000', {
+    placeholder: '012345-678'
+})
 
-    // Validação do formulário
-    $('#form').validate({
-        rules: {
-            nome: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            telefone: {
-                required: true
-            },
-            endereco: {
-                required: true
-            },
-            cep: {
-                required: true
-            },
-            cpf: {
-                required: true
-            }
+$('form').validate({
+    rules: {
+        nome: {
+            requerid: true
         },
-        messages: {
-            nome: "Por favor, insira seu nome.",
-            email: {
-                required: "Por favor, insira seu email.",
-                email: "Por favor, insira um email válido."
-            },
-            telefone: "Por favor, insira seu telefone.",
-            endereco: "Por favor, insira seu endereço.",
-            cep: "Por favor, insira seu CEP.",
-            cpf: "Por favor, insira seu CPF."
+        email: {
+            requerid: true,
+            email: true
         },
-        submitHandler: function (form) {
-            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-            form.reset();
-        }
-    });
-});
+        telefone: {
+            requerid: true
+        },
+        endereco: {
+            requerid: true
+        },
+        cep: {
+            requerid: true
+        },
+        cpf: {
+            requerid: true
+        },
+    },
+    submitHandler: function (form) {
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+        form.reset();
+    },
+    invalidHandler: function (form, validator) {
+        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    }
+})
